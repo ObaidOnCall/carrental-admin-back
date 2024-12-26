fb62c22a1309e100864919fa7c824b6c2b7cf11a
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2024-12-27 00:18:37 +0100

    Add Minio Client

--- src/main/java/ma/crm/carental/utils/UtilsConf.java
+++ src/main/java/ma/crm/carental/utils/UtilsConf.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.minio.MinioAsyncClient;
+import io.minio.MinioClient;
 import okhttp3.OkHttpClient;
 
 @Configuration
@@ -29,5 +31,23 @@ public class UtilsConf {
     @Bean
     public JsonConverter jsonConverter(@Autowired ObjectMapper objectMapper) {
         return new JsonConverter(objectMapper);
+    }
+
+    @Bean
+    public MinioClient minioClient () {
+
+        return MinioClient.builder()
+                .endpoint("https://storage-api.obayd.online")
+                .credentials("JiNzaG97TNPaOr", "8NczT1MlNL8Vjq3o2i0pzMYVtChf")
+                .build();
+    }
+
+    @Bean
+    public MinioAsyncClient minioAsyncClient () {
+
+        return MinioAsyncClient.builder()
+                                .endpoint("https://storage-api.obayd.online")
+                                .credentials("DcLfYpiX4FK1u", "akgSNqmV9IE8wR3pC70AX0j71Si")
+                                .build();
     }
 }
