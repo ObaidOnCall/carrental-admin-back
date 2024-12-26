5d73cd45f4217105400733f987aabe0a31a94d3e
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2024-12-27 00:17:38 +0100

    Add ClientDocs

--- src/main/java/ma/crm/carental/entities/ClientDocs.java
+++ src/main/java/ma/crm/carental/entities/ClientDocs.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,16 +41,22 @@ public class ClientDocs extends AbstractBaseEntity{
     @SequenceGenerator(name = "clientdoc_seq" , sequenceName = "clientdoc_id_seq"  , allocationSize = 1)
     private Long id ;
 
-    @Column(nullable = false , unique = true)
+    @Column(nullable = false)
     private String filename ;
 
-    @Column(nullable = false , unique = true)
+    @Column(nullable = false)
+    private double size ;
+
+    @Column(nullable = false)
+    private String contentType ;
+
+    @Column
     private String bucket ;
 
-    @Column(nullable = false , unique = true)
+    @Column
     private String region ;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Client client ;
 
