d2ff61d05377f43fee4463f286ea946d4c09c0b2
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-03 00:10:00 +0100

    Add hints config for native image

--- src/main/java/ma/crm/carental/CarentalApplication.java
+++ src/main/java/ma/crm/carental/CarentalApplication.java
@@ -3,12 +3,16 @@ package ma.crm.carental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+import ma.crm.carental.conf.HintsConfig;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @EnableAspectJAutoProxy
+@ImportRuntimeHints(HintsConfig.class)
 public class CarentalApplication {
 
 	public static void main(String[] args) {
